5784a12dbe8da1bb162a84cf271cb06a89c32a69
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-03-13 10:15:16 -0400

    reverted projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,54 +1,53 @@
-import React from "react";
-import { Row, Col, Button, Card, CardGroup } from "react-bootstrap";
-import { Link } from "react-router-dom";
-
-function Projects() {
-  return <Row></Row>;
-}
-
-export default Projects;
-
 // import React from "react";
-// import { useState, useEffect } from "react";
-// import { Link as RouterLink } from "react-router-dom";
-// import List from "../components/List";
+// import { Link } from "react-router-dom";
 
-// function Projects({ userName }) {
-//   const [loading, setLoading] = useState(true);
-//   const [projects, setProjects] = useState({});
-//   useEffect(() => {
-//     async function fetchData() {
-//       const data = await fetch(
-//         `https://api.github.com/users/${userName}/repos`
-//       );
-//       const result = await data.json();
-//       if (result) {
-//         setProjects(result);
-//         setLoading(false);
-//       }
-//     }
-//     fetchData();
-//   }, [userName]);
-
-//   return (
-//     <div className="Projects-container">
-//       <h2>Projects</h2>
-//       {loading ? (
-//         <span>Loading...</span>
-//       ) : (
-//         <div>
-//           <List
-//             items={projects.map((project) => ({
-//               field: project.name,
-//               value: (
-//                 <RouterLink url={project.html_url} title={project.html_url} />
-//               ),
-//             }))}
-//           />
-//         </div>
-//       )}
-//     </div>
-//   );
+// function Projects() {
+//   return <Row></Row>;
 // }
 
 // export default Projects;
+
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import List from "../components/List";
+
+function Projects({ userName }) {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState({});
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch(
+        `https://api.github.com/users/${userName}/repos`
+      );
+      const result = await data.json();
+      if (result) {
+        setProjects(result);
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [userName]);
+
+  return (
+    <div className="Projects-container">
+      <h2>Projects</h2>
+      {loading ? (
+        <span>Loading...</span>
+      ) : (
+        <div>
+          <List
+            items={projects.map((project) => ({
+              field: project.name,
+              value: (
+                <RouterLink url={project.html_url} title={project.html_url} />
+              ),
+            }))}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Projects;
