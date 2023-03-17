3a2ce97bbc549513dbf6290353306643b0f93900
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-03-17 12:32:00 -0400

    corrected images in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,21 @@
-import linkedin from "/Users/katymolony/Desktop/myProjects/portfolio2023/src/assets/linkedin.svg";
-import github from "/Users/katymolony/Desktop/myProjects/portfolio2023/src/assets/GitHub_icon.svg";
+import linkedin from "../assets/linkedin.svg";
+import github from "../assets/GitHub_icon.svg";
 import "./Footer.css";
 
 function Footer() {
   return (
     <footer>
-    <p>Find me on:</p>
-    <div className="mb-3 pt-0">
-      <a href="https://github.com/k8molony">
-        <img src={github} alt="Github" />
-      </a>
-      <a href="https://www.linkedin.com/in/k8molony">
-        <img src={linkedin} alt="LinkedIn" />
-      </a>
-    </div>
+      <p>Find me on:</p>
+      <div className="mb-3 pt-0">
+        <a href="https://github.com/k8molony">
+          <img src={github} alt="Github" />
+        </a>
+        <a href="https://www.linkedin.com/in/k8molony">
+          <img src={linkedin} alt="LinkedIn" />
+        </a>
+      </div>
     </footer>
-    );
+  );
 }
 
 export default Footer;
