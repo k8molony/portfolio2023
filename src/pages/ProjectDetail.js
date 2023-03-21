13b4c9d9bd51c3111a02c839c115d9dbb246ffd2
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-03-21 13:28:12 -0400

    change the projects page to remove github link

--- src/pages/ProjectDetail.js
+++ src/pages/ProjectDetail.js
@@ -1,33 +1,53 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+// import { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function ProjectDetail({ userName }) {
-  const [loading, setLoading] = useState(false);
-  const [project, setProject] = useState([]);
-  const { name } = useParams();
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetch(
-        `https://api.github.com/repos/k8molony/superFlix-client`
-      );
-      const result = await data.json();
-      if (result) {
-        setProject(result);
-        setLoading(false);
-      }
-    }
-    if (userName && name) {
-      fetchData();
-    }
-  }, [userName, name]);
+const ProjectDetail = (props) => {
+  let { title, index, thumbnail, desc } = props;
 
   return (
-    <div className="Project-container">
-      <h3>Project: {project.name}</h3>
-      {loading ? <span>Loading...</span> : <div></div>}
+    <div className="project-module">
+      <Link to={`/project-page/${index}`}>
+        <img src={`/bmps${thumbnail}`} alt={`${title} thumbnail`} />
+      </Link>
+      <h3>
+        <Link to={`/project-page/${index}`}></Link>
+      </h3>
+      <p>{desc}</p>
+      <Link className="project-link" to={`/project-page/${index}`}>
+        see project
+      </Link>
     </div>
   );
-}
+};
+
+// function ProjectDetail({ userName }) {
+//   const [loading, setLoading] = useState(false);
+//   const [project, setProject] = useState([]);
+//   const { name } = useParams();
+//   useEffect(() => {
+//     async function fetchData() {
+//       const data = await fetch(
+//         `https://api.github.com/repos/k8molony/superFlix-client`
+//       );
+//       const result = await data.json();
+//       if (result) {
+//         setProject(result);
+//         setLoading(false);
+//       }
+//     }
+//     if (userName && name) {
+//       fetchData();
+//     }
+//   }, [userName, name]);
+
+//   return (
+//     <div className="Project-container">
+//       <h3>Project: {project.name}</h3>
+//       {loading ? <span>Loading...</span> : <div></div>}
+//     </div>
+//   );
+// }
 
 export default ProjectDetail;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,53 +1,72 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import ProjectsJSON from "../json/projects.json";
 
-// function Projects() {
-//   return <Row></Row>;
-// }
+const Projects = (props) => {
+  const { changeLinkColor } = props;
 
-// export default Projects;
-
-import React from "react";
-import { useState, useEffect } from "react";
-import { Link as RouterLink } from "react-router-dom";
-import List from "../components/List";
-
-function Projects({ userName }) {
-  const [loading, setLoading] = useState(true);
-  const [projects, setProjects] = useState({});
   useEffect(() => {
-    async function fetchData() {
-      const data = await fetch(
-        `https://api.github.com/users/${userName}/repos`
-      );
-      const result = await data.json();
-      if (result) {
-        setProjects(result);
-        setLoading(false);
-      }
-    }
-    fetchData();
-  }, [userName]);
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    changeLinkColor("grey");
+  }, [changeLinkColor]);
+
+  const { xml } = props;
 
   return (
-    <div className="Projects-container">
-      <h2>Projects</h2>
-      {loading ? (
-        <span>Loading...</span>
-      ) : (
-        <div>
-          <List
-            items={projects.map((project) => ({
-              field: project.name,
-              value: (
-                <RouterLink url={project.html_url} title={project.html_url} />
-              ),
-            }))}
-          />
-        </div>
-      )}
-    </div>
+    <section className="Projects-container">
+      <div className="thunbnails">
+        <h2>Projects</h2>
+        {xml.map((item, key) => {
+          return <div key={key} className="thumbnail-container"></div>;
+        })}
+      </div>
+    </section>
   );
-}
+};
+
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { Link as RouterLink } from "react-router-dom";
+// import List from "../components/List";
+
+// function Projects({ userName }) {
+//   const [loading, setLoading] = useState(true);
+//   const [projects, setProjects] = useState({});
+//   useEffect(() => {
+//     async function fetchData() {
+//       const data = await fetch(
+//         `https://api.github.com/users/${userName}/repos`
+//       );
+//       const result = await data.json();
+//       if (result) {
+//         setProjects(result);
+//         setLoading(false);
+//       }
+//     }
+//     fetchData();
+//   }, [userName]);
+
+//   return (
+//     <div className="Projects-container">
+//       <h2>Projects</h2>
+//       {loading ? (
+//         <span>Loading...</span>
+//       ) : (
+//         <div>
+//           <List
+//             items={projects.map((project) => ({
+//               field: project.name,
+//               value: (
+//                 <RouterLink url={project.html_url} title={project.html_url} />
+//               ),
+//             }))}
+//           />
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
 
 export default Projects;
