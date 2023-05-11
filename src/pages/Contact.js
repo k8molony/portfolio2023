586453fac879a35f5a6d49daf448ae0078f45630
Katy Molony <85511799+k8molony@users.noreply.github.com>
2023-05-11 16:04:32 -0400

    add margin to projects

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 import "./Contact.css";
 
 const FORM_ENDPOINT =
@@ -35,47 +36,31 @@ const ContactForm = () => {
         <p>Or fill out the form below:</p>
       </div>
 
-      <form
+      <Form
         action={FORM_ENDPOINT}
         onSubmit={handleSubmit}
         method="POST"
         target="_blank"
       >
-        <div className="mb-3 pt-10">
-          <input
-            type="text"
-            placeholder="Your name"
-            name="name"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-            required
-          />
-        </div>
-        <div className="mb-3 pt-0">
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-            required
-          />
-        </div>
-        <div className="mb-3 pt-0">
-          <textarea
-            placeholder="Your message"
-            name="message"
-            className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
-            required
-          />
-        </div>
-        <div className="mb-3 pt-0">
-          <button
-            className="bg-cyan-700 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-            type="submit"
-          >
-            Send a message
-          </button>
-        </div>
-      </form>
+        <Form.Group className="mb-3 pt-10" controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Your name" required />
+        </Form.Group>
+
+        <Form.Group className="mb-3 pt-0" controlId="formEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Email" required />
+        </Form.Group>
+
+        <Form.Group className="mb-3 pt-0" controlId="formMessage">
+          <Form.Label>Your message:</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Send a message
+        </Button>
+      </Form>
     </>
   );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,60 +13,52 @@ export default function Projects() {
   }, []);
 
   return (
-    <Container className="project-container">
-      <Row>
-        {projects?.map((project) => (
-          <Col key={project.id}>
-            <CardGroup>
-              <Card className="project-card">
-                <Card.Img variant="top" type="fluid" src={project.thumbnail} />
-                <Card.Body>
-                  <Card.Title>{project.Title}</Card.Title>
-                  <div className="project_programs">
-                    <span className="project_line_value">
-                      {project.programs}
-                    </span>
-                  </div>
+    <Row>
+      {projects?.map((project) => (
+        <Col key={project.id}>
+          <CardGroup>
+            <Card className="project-card">
+              <Card.Img variant="top" type="fluid" src={project.thumbnail} />
+              <Card.Body>
+                <Card.Title>{project.Title}</Card.Title>
+                <div className="project_programs">
+                  <span className="project_line_value">{project.programs}</span>
+                </div>
 
-                  <div className="project_link">
-                    <span className="project_label"></span>
-                    <Link
-                      to={project.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Button variant="link">{project.title}</Button>
-                    </Link>
-                  </div>
+                <div className="project_link">
+                  <span className="project_label"></span>
+                  <Link
+                    to={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button variant="link">{project.title}</Button>
+                  </Link>
+                </div>
 
-                  <div className="github_link">
-                    <span className="project_label"></span>
-                    <Link
-                      to={project.repo}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <Button variant="link">Github Repository</Button>
-                    </Link>
-                  </div>
+                <div className="github_link">
+                  <span className="project_label"></span>
+                  <Link
+                    to={project.repo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button variant="link">Github Repository</Button>
+                  </Link>
+                </div>
 
-                  <div style={{ marginTop: "12px" }}>
-                    <Link to={`/projects/${project.id}`}>
-                      <Button
-                        className="button-info"
-                        variant="outline"
-                        size="sm"
-                      >
-                        More info
-                      </Button>
-                    </Link>
-                  </div>
-                </Card.Body>
-              </Card>
-            </CardGroup>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+                <div style={{ marginTop: "12px" }}>
+                  <Link to={`/projects/${project.id}`}>
+                    <Button className="button-info" variant="outline" size="sm">
+                      More info
+                    </Button>
+                  </Link>
+                </div>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      ))}
+    </Row>
   );
 }
